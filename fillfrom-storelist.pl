388c74167512a7bb2b2fec914c128be83d5c202f
Alja Mrak Tadel <amraktadel@ucsd.edu>
2014-02-01 10:26:14 -0800

    fix xrdfragcp path.

--- fillfrom-storelist.pl
+++ fillfrom-storelist.pl
@@ -21,7 +21,7 @@ for (my $count = 0; $count < 50; $count++) {
    my $randomline=$array[rand @array];
    chomp $randomline;
 
-   my $cmd = "/root/sim/xrdfragcp --cmsclientsim 720000000 300 3000 --vread 128 root://xrootd-proxy.t2.ucsd.edu//store/".$randomline." &";
+   my $cmd = "$ENV{HOME}/xrd/sim/xrdfragcp --cmsclientsim 720000000 300 3000 --vread 128 root://xrootd-proxy.t2.ucsd.edu//store/".$randomline." &";
 
    print (localtime, " $cmd \n");
     system(" $cmd");
